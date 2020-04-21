e117ea17fad9b2cae0c683ffacdc31dc3ae6a6e5
James Cornelius <jcorn2@umbc.edu>
2020-04-21 19:18:18 -0400

    Ejected app and added penguin to player component

--- src/components/mainArea/player/Player.jsx
+++ src/components/mainArea/player/Player.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Container,
+} from '@material-ui/core';
 import { ReactComponent as Penguin } from '../../../assets/penguin.svg';
 
+const useStyles = makeStyles({
+    penguin: {
+        height: '475px',
+        width: '375px',
+    },
+    player: {
+        gridArea: 'player1',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
 function Player() {
+    const classes = useStyles();
+
     return (
-        <Penguin />
+        <Container className={classes.player}>
+            <Penguin className={classes.penguin} />
+        </Container>
     );
 }
 
