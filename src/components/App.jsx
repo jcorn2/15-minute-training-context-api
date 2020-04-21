513e721b2799949e4c827d18e9445c0850c5d3bc
James Cornelius <jcorn2@umbc.edu>
2020-04-21 17:44:31 -0400

    Added material-ui and releated packages

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'typeface-roboto';
+import 'typeface-roboto-mono';
+import {
+  Container,
+} from '@material-ui/core';
+
+function App() {
+  return (
+    <Container>
+      Hello World
+    </Container>
+  );
+}
+
+export default App;
