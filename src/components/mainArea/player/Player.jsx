68711c1c50cc89ff0ced5acf1af68e459418caa6
James Cornelius <jcorn2@umbc.edu>
2020-04-22 18:12:52 -0400

    Added game icons to player

--- src/components/mainArea/player/Player.jsx
+++ src/components/mainArea/player/Player.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import {
   Container,
+  IconButton,
 } from '@material-ui/core';
-import { ReactComponent as Penguin } from '../../../assets/penguin.svg';
+import { ReactComponent as Rock } from '@fortawesome/fontawesome-free/svgs/regular/hand-rock.svg';
+import { ReactComponent as Paper } from '@fortawesome/fontawesome-free/svgs/regular/hand-paper.svg';
+import { ReactComponent as Scissors } from '@fortawesome/fontawesome-free/svgs/regular/hand-scissors.svg';
+import { ReactComponent as Penguin } from 'assets/penguin.svg';
 
 const useStyles = makeStyles({
+    gameIcon: {
+        height: '84px',
+        width: '84px',
+    },
+    gameIconsContainer: {
+        display: 'flex',
+        justifyContent: 'space-around',
+    },
     penguin: {
         height: '475px',
         width: '375px',
@@ -13,6 +25,7 @@ const useStyles = makeStyles({
     player: {
         gridArea: 'player1',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -24,6 +37,17 @@ function Player() {
     return (
         <Container className={classes.player}>
             <Penguin className={classes.penguin} />
+            <Container className={classes.gameIconsContainer}>
+                <IconButton>
+                    <Rock className={classes.gameIcon} />
+                </IconButton>
+                <IconButton>
+                    <Paper className={classes.gameIcon} />
+                </IconButton>
+                <IconButton>
+                    <Scissors className={classes.gameIcon} />
+                </IconButton>
+            </Container>
         </Container>
     );
 }
